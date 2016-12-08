bdddba8273b480474966a813c19ff25d63ce5547
张硕 <zhangshuo@zhangshuodeMBP.lan>
2016-12-08 15:42:18 +0800

    上传远端分支

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,12 +1,3 @@
 class Users::RegistrationsController < Devise::RegistrationsController
 
-  protected
-
-  def after_sign_in_path_for(resume)
-    user_resume_preview_path
-    end
-
-    def after_update_path_for(resume)
-    user_resume_preview_path
-    end
 end
