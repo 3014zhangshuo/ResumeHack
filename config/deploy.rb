d889424cd332e1c0e35c549d6f81c116638c802d
3014zhangshuo <3014zhangshuo@gmail.com>
2017-01-07 10:32:25 +0800

    modify deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,15 @@
 # config valid only for current version of Capistrano
 lock "3.7.1"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :application, "ResumeHack"
+set :repo_url, "git@github.com:ResumeHack/ResumeHack.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
 # set :deploy_to, "/var/www/my_app_name"
+set :deploy_to, "/home/apps/resumes"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
@@ -19,6 +20,7 @@ set :repo_url, "git@example.com:me/my_repo.git"
 
 # Default value for :pty is false
 # set :pty, true
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 
 # Default value for :linked_files is []
 # append :linked_files, "config/database.yml", "config/secrets.yml"
@@ -28,6 +30,7 @@ set :repo_url, "git@example.com:me/my_repo.git"
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle')
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
